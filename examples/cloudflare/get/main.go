971b98b4e02710c18c9e9873a01f8fd0ae2fce9d
warjiang <1096409085@qq.com>
2025-07-14 13:46:38 +0800

    feat(cloudflare): add new examples for cloudflare
    Signed-off-by: warjiang <1096409085@qq.com>

---
+++ examples/cloudflare/get/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/warjiang/cloudflare-tunnel-operator/pkg/cloudflare"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	apiToken := os.Getenv("API_TOKEN")
+	accountID := os.Getenv("ACCOUNT_ID")
+	client, err := cloudflare.NewClient(
+		accountID,
+		cloudflare.WithAPIToken(apiToken),
+	)
+	if err != nil {
+		log.Fatal("Error creating client")
+	}
+
+	ctx := context.TODO()
+	name, err := client.GetTunnelByName(ctx, "my-tunnel")
+	if err != nil {
+		log.Fatalf("Error creating tunnel %v\n", err)
+	}
+	fmt.Printf("%+v", name)
+}
